36db0a0a4a194409922569ddfcf2f5980f622a2c
xinjie_guo <xinjie_guo@xinyan.com>
2019-11-28 17:24:02 +0800

    ..

--- src/main/java/gxj/study/spring/bean/MyBean.java
+++ src/main/java/gxj/study/spring/bean/MyBean.java
@@ -1,14 +1,17 @@
 package gxj.study.spring.bean;
 
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
 /**
  * @author xinjie_guo
  * @version 1.0.0 createTime:  2019/11/7 15:31
  * @description
  */
-//@Component
+
 public class MyBean{
     public MyBean(){
         System.out.println("MyBean构造方法");
     }
-    public String name;
+    public String name = "init";
 }
